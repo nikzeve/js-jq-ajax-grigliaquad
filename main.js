c2c7b3a23fdc3465a8e5368d8643dac843d7778d
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-05-19 17:50:05 +0200

    push quasi finale

--- main.js
+++ main.js
@@ -4,29 +4,42 @@
 // In entrambi i casi, andiamo ad inserire nel quadrato il numero restituito dall'api.
 // BONUS: generare la griglia in jQuery utilizzando handlebars
 
-// attivo il click su ciascun quadrato
-$('.quadrato').click(function(){
+$(document).ready(function() {
+
+    var template_html = $('#entry-template').html();
+    var template_function = Handlebars.compile(template_html);
+    var html_finale = template_function();
+    for (var i = 0; i < 36; i++) {
+        $('#griglia').append(html_finale);
+    }
 
 
-    var quadrato_corrente = $(this);
 
-    $.ajax({
-        url: 'https://flynn.boolean.careers/exercises/api/random/int',
-        method: 'GET',
-        success: function(data){
+    $('.quadrato').click(function(){
 
-            var numero_random = data.response;
+        // qui dovrei fare un controllo per vedere se il quadrato è già stato cliccato
 
-            console.log(numero_random);
+        var quadrato_corrente = $(this);
 
-            if (numero_random <= 5) {
-                quadrato_corrente.addClass('giallo');
-            } else {
-                quadrato_corrente.addClass('verde');
+        $.ajax({
+            url: 'https://flynn.boolean.careers/exercises/api/random/int',
+            method: 'GET',
+            success: function(data){
+
+                var numero_random = data.response;
+
+                console.log(numero_random);
+
+                if (numero_random <= 5) {
+                    quadrato_corrente.addClass('giallo');
+                } else {
+                    quadrato_corrente.addClass('verde');
+                }
+
+                quadrato_corrente.append(numero_random);
             }
 
-            quadrato_corrente.append(numero_random);
-        }
+        });
 
     });
 
