2663e7d069f3f046e3d4c49da16867061d1311e9
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-05-19 17:29:15 +0200

    push intermedia

--- main.js
+++ main.js
@@ -2,4 +2,32 @@
 // Ad ogni click su un quadratino, parte una richiesta ajax per recuperare un numero random tra 1 e 9.
 // Se il numero restituito dall'api è <= 5, il quadrato su cui l'utente ha cliccato diventa giallo; se invece il numero restituito dall'api è > 5, il quadrato su cui l'utente ha cliccato diventa verde.
 // In entrambi i casi, andiamo ad inserire nel quadrato il numero restituito dall'api.
-// BONUS: generare la griglia in jQuery utilizzando handlebars 
+// BONUS: generare la griglia in jQuery utilizzando handlebars
+
+// attivo il click su ciascun quadrato
+$('.quadrato').click(function(){
+
+
+    var quadrato_corrente = $(this);
+
+    $.ajax({
+        url: 'https://flynn.boolean.careers/exercises/api/random/int',
+        method: 'GET',
+        success: function(data){
+
+            var numero_random = data.response;
+
+            console.log(numero_random);
+
+            if (numero_random <= 5) {
+                quadrato_corrente.addClass('giallo');
+            } else {
+                quadrato_corrente.addClass('verde');
+            }
+
+            quadrato_corrente.append(numero_random);
+        }
+
+    });
+
+});
